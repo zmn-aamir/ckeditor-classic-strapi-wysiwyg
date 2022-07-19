bba58f96bfb4c867f3a1ed1a431f50602951da2c
Amir Ashraf <>
2022-07-19 14:38:06 +0500

    new element

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -187,4 +187,33 @@ ClassicEditor.defaultConfig = {
 	language: 'en'
 };
 
+// This SVG file import will be handled by webpack's raw-text loader.
+// This means that imageIcon will hold the source SVG.
+import imageIcon from '@ckeditor/ckeditor5-core/theme/icons/image.svg';
+import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview';
+
+class InsertImage extends Plugin {
+    init() {
+        const editor = this.editor;
+
+        editor.ui.componentFactory.add( 'insertImage', locale => {
+            const view = new ButtonView( locale );
+
+            view.set( {
+                label: 'Insert image',
+                icon: imageIcon,
+                tooltip: true
+            } );
+
+            // Callback executed once the image is clicked.
+            view.on( 'execute', () => {
+                const imageURL = prompt( 'Image URL' );
+            } );
+
+            return view;
+        } );
+    }
+}
+
  
