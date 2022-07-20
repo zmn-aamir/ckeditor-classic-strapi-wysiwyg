a694388cbef09fcd367fe42261d5d3e59327ba07
Amir Ashraf <>
2022-07-20 14:41:43 +0500

    new element

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -438,9 +438,9 @@ ClassicEditor.defaultConfig = {
 // import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 // import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 // import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
-import Heading from '@ckeditor/ckeditor5-heading/src/heading';
-import List from '@ckeditor/ckeditor5-list/src/list';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+// import Heading from '@ckeditor/ckeditor5-heading/src/heading';
+// import List from '@ckeditor/ckeditor5-list/src/list';
+// import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 import { toWidget, toWidgetEditable } from '@ckeditor/ckeditor5-widget/src/utils';
